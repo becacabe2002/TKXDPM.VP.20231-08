92d885097caa32302d36e4aab8a1109aaa4d1f13
Tu Ngo Minh <tungominh.hust@gmail.com>
2023-11-21 21:25:09 +0700

    Update data repo

--- src/main/java/com/grp08/capstoneprojectg08/repository/BaseRepo.java
+++ src/main/java/com/grp08/capstoneprojectg08/repository/BaseRepo.java
@@ -5,10 +5,12 @@ import com.grp08.capstoneprojectg08.util.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 public abstract class BaseRepo {
     protected Connection dbConnection = DatabaseConnection.getConnection();
     protected PreparedStatement ppStatement = null;
 
+    protected Statement statement = null;
     protected ResultSet resultSet = null;
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.grp08.capstoneprojectg08 {
     requires javafx.fxml;
     requires java.sql;
     requires lombok;
+    requires mysql.connector.j;
 
 
     opens com.grp08.capstoneprojectg08 to javafx.fxml;
